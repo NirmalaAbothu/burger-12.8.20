cfbbbd50da40b49110da9a1fcc4bd5592307870c
Nirmala <nirmala_ira@hotmail.com>
2020-12-10 13:19:27 -0800

    commit the project folders with completed functionalities

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('ChickenSandwitch',true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('FishSandwitch',true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('CheeseSandwitch',true);
---
+++ public/assets/js/burger.js
@@ -0,0 +1,54 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+     $(".eatburger").on("click", function (event) {
+          var id = $(this).data("id");
+          var newDevoure = $(this).data("newdevoure");
+
+          var newDevouredState = {
+               devoured: newDevoure,
+          };
+
+          // Send the PUT request.
+          $.ajax("/api/burgers/" + id, {
+               type: "PUT",
+               data: newDevouredState,
+          }).then(function () {
+               console.log("changed sleep to", newDevoure);
+               // Reload the page to get the updated list
+               location.reload();
+          });
+     });
+
+     $(".create-form").on("submit", function (event) {
+          // Make sure to preventDefault on a submit event.
+          event.preventDefault();
+
+          var newBurger = {
+               name: $("#ca").val().trim(),
+               devoured: 0,
+          };
+
+          // Send the POST request.
+          $.ajax("/api/burgers", {
+               type: "POST",
+               data: newBurger,
+          }).then(function () {
+               console.log("Added new burger");
+               // Reload the page to get the updated list
+               location.reload();
+          });
+     });
+
+     //  $(".delete-cat").on("click", function (event) {
+     //       var id = $(this).data("id");
+
+     //       // Send the DELETE request.
+     //       $.ajax("/api/cats/" + id, {
+     //            type: "DELETE",
+     //       }).then(function () {
+     //            console.log("deleted cat", id);
+     //            // Reload the page to get the updated list
+     //            location.reload();
+     //       });
+     //  });
+});
