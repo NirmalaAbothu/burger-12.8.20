c9780b8694cac0e1aed6792abdb6ed2e0b879e21
Nirmala <nirmala_ira@hotmail.com>
2020-12-12 01:06:53 -0800

    removed commented lines

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,6 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-// var connection = mysql.createConnection({
-//      host: "localhost",
-//      port: 3306,
-//      user: "root",
-//      password: process.env.PW,
-//      database: "burger_db",
-// });
-
 if (process.env.JAWSDB_URL) {
      connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
